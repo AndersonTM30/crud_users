35fd00ca7973ab8b7039bbb88708305c27d1cbfc
root <root@pop-os.localdomain>
2023-11-07 23:44:13 -0300

    feat: adicionando hash no password antes de salvar no banco de dados

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,20 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    const saltOrRounds = 10;
+    createUserDto.password = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds,
+    );
+    console.log(typeof createUserDto.password);
+    // return createUserDto;
     return this.prisma.user.create({ data: createUserDto });
   }
 
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+      },
+    });
   }
 
   findOne(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
